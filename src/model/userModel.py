c79125f7fbb8e947f9488015224878fb81c683f7
HoshinoTouko <oranly@hotmail.com>
2017-08-31 00:53:35 +0800

    Reservation class.

--- src/model/userModel.py
+++ src/model/userModel.py
@@ -1,10 +1,64 @@
 '''A model for user functions'''
 from .db import DB
+from .common import TimeParser
 
-def auth_user_and_pass(username, password):
-    '''Auth user by name and pass'''
-    return True
-    for item in data:
-        if item['name'] == username and item['pass'] == password:
-            return True
-    return False
+
+class Reservation:
+    '''A class to resolve reservation request'''
+    @staticmethod
+    def check_data(data):
+        '''Check if the data is valid'''
+        import re
+        '''
+        Sample data
+        {
+            'name': 'asdasd', 'telephone': '123213',
+            'starttime': '08:00', 'endtime': '10:00',
+            'reservdate': '30 August, 2017', 'room': '201',
+            'forwhat': 'wkdfjenf'
+        }
+        '''
+        regularly_list = {
+            'name': '^[\u4E00-\u9FA5A-Za-z0-9]+$',
+            'telephone': '^(1[3|4|5|6|7|8|9])\d{9}$',
+            'starttime': '^\d{2}:\d{2}',
+            'endtime': '^\d{2}:\d{2}',
+            'reservdate': '^\d{4}-\d{1,2}-\d{1,2}',
+            'room': '^[\u4E00-\u9FA5A-Za-z0-9]+$',
+            'forwhat': '^[\u4E00-\u9FA5A-Za-z0-9]+$'
+        }
+        keyname_list = {
+            'name': '名字',
+            'telephone': '电话',
+            'starttime': '开始时间',
+            'endtime': '结束时间',
+            'reservdate': '预定日期',
+            'room': '房间',
+            'forwhat': '用途'
+        }
+        for key in data.keys():
+            valid = False
+            try:
+                valid = re.match(regularly_list[key], data[key])
+            except BaseException as error:
+                valid = False
+                print('Regular parse error: ' + str(error))
+
+            if valid:
+                continue
+            else:
+                return False, keyname_list[key] + '出错啦'
+        return True, 'OK'
+
+    @staticmethod
+    def reserve(data):
+        '''Reserve room'''
+        status, tip = Reservation.check_data(data)
+        if status:
+            pass
+        return status, tip
+
+    @staticmethod
+    def auth_user(username, authinfo):
+        '''Auth user by name and pass'''
+        return True
--- src/views/api.py
+++ src/views/api.py
@@ -1,13 +1,33 @@
 '''Index view module'''
 
-from flask import Blueprint, render_template, session, request
+from ..model import common, userModel
+from flask import Blueprint, session, request
 
 API = Blueprint('api', __name__)
 
 @API.route('/add_reservation', methods=["GET", "POST"])
 def add_reservation():
-    '''Do some stuff'''
-    print(request.form["name"])
+    '''Backend check data and submit.'''
+    return_data = {
+        'status': 0,
+        'data': [],
+        'tip': ''
+    }
+    data = request.form.to_dict()
     if request.method == 'POST':
-        return str(request.form)
-    return 'Nothing'
+        # Log data
+        print('API view get POST data: ' + str(data))
+        # Change date format
+        data['reservdate'] = common.TimeParser.parse_date(data['reservdate'])
+        # Reserve
+        status, tip = userModel.Reservation.reserve(data)
+        # Set json
+        return_data['status'] = 1 if status else -1
+        return_data['tip'] = tip
+    else:
+        # If not POST method, set json
+        return_data['status'] = 0
+        return_data['tip'] = 'No POST data.'
+        print('No POST data.')
+    # Javascript cannot parse ', so you must parse "
+    return str(return_data).replace("'", '"')
