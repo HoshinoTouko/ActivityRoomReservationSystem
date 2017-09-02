ed9ad069c44ae176fc466900beeb3d04fc9fd066
HoshinoTouko <oranly@hotmail.com>
2017-09-03 01:25:39 +0800

    Add get_future_reservation and get_past_reservation route. They will be modified if i am happy.

--- src/views/api.py
+++ src/views/api.py
@@ -5,6 +5,16 @@ from flask import Blueprint, session, request
 
 API = Blueprint('api', __name__)
 
+@API.route('/get_future_reservation')
+def get_future_reservation():
+    '''Get future reservation'''
+    pass
+
+@API.route('/get_past_reservation')
+def get_past_reservation():
+    '''Get past reservation'''
+    pass
+
 @API.route('/add_reservation', methods=["GET", "POST"])
 def add_reservation():
     '''Backend check data and submit.'''
@@ -22,8 +32,11 @@ def add_reservation():
         user.show()
         # Reserve
         status, tip = user.check()
+        # If check() passed, go to the next step
+        if status == 1:
+            status, tip = user.submit()
         # Set json
-        return_data['status'] = 1 if status else -1
+        return_data['status'] = 1 if status == 1 else -1
         return_data['tip'] = tip
     else:
         # If not POST method, set json
