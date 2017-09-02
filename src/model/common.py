50c789a218a3362dfcf9f8b9bd32069c6d663023
HoshinoTouko <oranly@hotmail.com>
2017-09-03 01:22:04 +0800

    Add list_filter and get_date_range function. Update TimeParser to Common.

--- src/model/common.py
+++ src/model/common.py
@@ -1,7 +1,8 @@
 '''Some common functions'''
 
-class TimeParser:
-    '''A class for resolve time and date text'''
+
+class Common:
+    '''A class for handling date'''
     @staticmethod
     def parse_date(date_text):
         '''Parse date text, like 30 August, 2017'''
@@ -9,3 +10,30 @@ class TimeParser:
         result = time.strptime(date_text, '%d %B, %Y')
         result_text = time.strftime('%Y-%m-%d', result)
         return result_text
+
+    @staticmethod
+    def get_date_range(area, offset=0):
+        '''Return a list of a range of date string list.'''
+        import datetime
+        result = []
+        now = datetime.datetime.now() + datetime.timedelta(days=offset)
+        for i in range(0, area, 1 if area >= 0 else -1):
+            result.append((now + datetime.timedelta(days=i)).strftime('%Y-%m-%d'))
+        return result
+
+    @staticmethod
+    def list_filter(origin_list, key, value):
+        '''A list filter'''
+        result = []
+        if not isinstance(value, list):
+            value = [value]
+        for item in origin_list:
+            try:
+                if item[key] in value:
+                    result.append(item)
+                    continue
+                continue
+            except BaseException:
+                continue
+            continue
+        return result
