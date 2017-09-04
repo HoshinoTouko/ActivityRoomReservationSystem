d04557a778f6208903d30445a284038475b7345c
HoshinoTouko <oranly@hotmail.com>
2017-09-04 20:21:57 +0800

    # For safety reason, delete phone column.

--- src/model/Reservation.py
+++ src/model/Reservation.py
@@ -11,12 +11,30 @@ class Reservation:
     @staticmethod
     def get_future_reservation():
         '''Return a list about future 14 days reservation.'''
-        return Common.list_filter(Reservation.get_all_reservation(), 'reservdate', Common.get_date_range(14))
+        return Common.list_filter(
+            Reservation.get_all_reservation_safe(),
+            'reservdate',
+            Common.get_date_range(14)
+        )
 
     @staticmethod
     def get_past_reservation():
         '''Return a list about past 14 days reservation.'''
-        return Common.list_filter(Reservation.get_all_reservation(), 'reservdate', Common.get_date_range(-14))
+        return Common.list_filter(
+            Reservation.get_all_reservation_safe(),
+            'reservdate',
+            Common.get_date_range(-14)
+        )
+
+    @staticmethod
+    def get_all_reservation_safe():
+        '''Get a list about all reservation.'''
+        database = Reservation.get_db()
+        # For safety reason, delete phone column.
+        result = database.select('Reservation')
+        for item in result:
+            del item['telephone']
+        return result
 
     @staticmethod
     def get_all_reservation():
